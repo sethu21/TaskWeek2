ec2d0786db91ca9b2dc2a2cc0da8300aa106af97
sethu21 <sethu2196@gmail.com>
2023-01-18 16:06:38 +0200

    commit

---
+++ src/task11/MatrixAns.java
@@ -0,0 +1,10 @@
+package task11;
+
+import java.util.Random;
+
+public class MatrixAns {
+    public static void main(String[] args) {
+        Matrix ans = new Matrix("rows","")
+        System.out.println();
+    }
+}
